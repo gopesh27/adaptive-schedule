39a28fe16ae8dab97992628a1fe31a645766fa5f
gopesh27 <gopeshsenthil2006@gmail.com>
2025-09-27 20:50:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.markdown("""
     <style>
     /* Main background */
     .stApp {
-        background-color: #000000;
+        background-color: #FFF8DC;
         font-family: 'Segoe UI', sans-serif;
     }
 
